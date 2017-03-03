fba5c4ecc4ca14d542d28d817fb58656eaccce34
Victor Hsiang <vhsiangyl@gmail.com>
2017-03-02 16:02:04 -0800

    json in cloud and added some comments

--- localization/Loc.py
+++ localization/Loc.py
@@ -8,18 +8,23 @@ import Loc_wifi_start
 import Loc_wifi_cont
 
 print('Welcome to Localization w/ only wifi strengths\n')
+#Have the user enter a command
 control_signal = raw_input('Operation ([s]-start, [c]-continue, [q]-quit): ')
 
+#Initialize a map dictionary and a reference point dictionary
 [map_dict,list_dict_point] = Loc_wifi_ini.Loc_wifi_ini()
 
 while 1:
 	pass
+	#if user chooses start, use starting code
 	if control_signal == 's':
 		Loc_wifi_start.Loc_wifi_start(map_dict,list_dict_point)
 		break
+	#if user chooses continue, use continuing code
 	elif control_signal == 'c':
 		Loc_wifi_cont.Loc_wifi_cont(map_dict,list_dict_point)
 		break
+	#if user chooses quit, quit
 	elif control_signal == 'q':
 		print ("Exit localization")
 		break
