6ebbd7c40dc969e04b96face8fd1e0a518438172
William Chen <billyskc@ucla.edu>
2017-03-03 17:52:48 -0800

    add shell script

--- localization/Loc.py
+++ localization/Loc.py
@@ -4,24 +4,16 @@ import scipy.stats
 import os
 import sys
 
-os.system(command)
 import Loc_wifi_ini
 import Loc_wifi_start
 import Loc_wifi_cont
 
 print('Welcome to Localization w/ only wifi strengths\n')
-<<<<<<< HEAD
-=======
-#Have the user enter a command
-control_signal = raw_input('Operation ([s]-start, [c]-continue, [q]-quit): ')
-
 #Initialize a map dictionary and a reference point dictionary
->>>>>>> a82867c514a1ca9556718c7d8cc4ed4d01165987
 [map_dict,list_dict_point] = Loc_wifi_ini.Loc_wifi_ini()
 
 
-
-os.system('chmod +x filename.sh')
+os.system('chmod +x testPoint.sh')
 
 
 while 1:
@@ -31,17 +23,11 @@ while 1:
 	if control_signal == 's':
 		os.system('sh testPoint.sh')
 		Loc_wifi_start.Loc_wifi_start(map_dict,list_dict_point)
-<<<<<<< HEAD
 	elif control_signal == 'c':
-		Loc_wifi_cont.Loc_wifi_cont(map_dict,list_dict_point)
-=======
-		break
 	#if user chooses continue, use continuing code
-	elif control_signal == 'c':
+		os.system('sh testPoint.sh')
 		Loc_wifi_cont.Loc_wifi_cont(map_dict,list_dict_point)
-		break
 	#if user chooses quit, quit
->>>>>>> a82867c514a1ca9556718c7d8cc4ed4d01165987
 	elif control_signal == 'q':
 		print ("Exit localization")
 		break
--- localization/testPoint.sh
+++ localization/testPoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-FILENAME= "scan_result.txt"
+FILENAME= "scan_results.txt"
 
-iwlist wlan0 scan | grep 'Address:\|Signal' | sed 's/.*Address: //; s/.*\([0-9]\{2\}\) dBm/\1/' | sed 'N; s/\n/ /' | sort > test_result.txt
-
-python Loc_wifi.py
+iwlist wlan0 scan | grep 'Address:\|Signal' | sed 's/.*Address: //; s/.*\([0-9]\{2\}\) dBm/\1/' | sed 'N; s/\n/ /' | sort > FILENAME
