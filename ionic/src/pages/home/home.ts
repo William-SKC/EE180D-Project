47103c8b5df27ed27c5df3627f7c258d4fa4a672
Victor Hsiang <vhsiangyl@gmail.com>
2017-03-07 21:30:33 -0800

    fix sh bug and commented ionic code

--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -13,25 +13,14 @@ export class HomePage {
   loc: FirebaseListObservable<any>
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, angFire: AngularFire) {
-  	//var ref = firebase.database.ref("Users");
-  	// ref.child("Users").orderByChild("Toggle").equalTo("on").once("value",function(snapshot) {
-    // var userData = snapshot.val();
-    // if (userData){
-    //   console.log("exists!");
-    // }
-// });
-
+  	//Declares angFire objects on the class HomePage and directs them to their specific node.
   	this.users = angFire.database.list('/Users');
-  	this.loc = angFire.database.list('/Users/Locations');
-
-  	// this.users.subscribe(
-  	// 	scan => this.loc.push({
-  	// 		Location: "Position 1"
-  	// 	})
-  	// 	);
+  	this.loc = angFire.database.list('/Users/Locations')
   }
 
+  //This is a function that provides the logic for the button for adding users.
   addUser():void {
+    //Create an object that is an alertCtrl with the following title, message, and inputs
   	let prompt = this.alertCtrl.create({
   		title: 'Welcome to Hospitably',
   		message: 'Enter your name and position',
@@ -53,6 +42,11 @@ export class HomePage {
   			}
   		},
   		{
+        //When the navigate button is pressed, push the inputs into the angFire object that is linked to a node
+        //Need to know when navigate is pressed, then need to call the command to calculate the current position
+        //I can't call python in TS, so my options are to trigger the python script through the firebase by having an observer in node.js to watch 
+        //for a new user and to call the python script when this happens. I can also just write everything in js through node.js but i think it'd be 
+        //easier to just do the first option
   			text: "Navigate",
   			handler: data => {
   				this.users.push({
