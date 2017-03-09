866235de59976669a13e3dc569d53813e483d2ee
Victor Hsiang <vhsiangyl@gmail.com>
2017-03-09 07:44:11 -0800

    with sample python listener in sample.py

--- localization/testPoint.sh
+++ localization/testPoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-FILENAME
-"scan_results.txt"
+FILENAME="scan_results.txt"
 
 iwlist wlan0 scan | grep 'Address:\|Signal' | sed 's/.*Address: //; s/.*\([0-9]\{2\}\) dBm/\1/' | sed 'N; s/\n/ /' | sort > "scan_results.txt"
 
