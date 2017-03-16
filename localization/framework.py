3e1f56197d8ef415c7e9030a13a4a56229bfc75b
William Chen <billyskc@ucla.edu>
2017-03-16 13:33:13 -0700

    complete test

--- localization/framework.py
+++ localization/framework.py
@@ -16,46 +16,24 @@ from threading import Thread
 from multiprocessing import Process, Array, Lock, Manager, Value
 from functools import partial
 
-'''
-def add(lock, curr_state):
-	while 1:
-		with lock:
-			curr_state[0] = curr_state[0]+5
-			curr_state[2] = curr_state[2]+50
-			print('Add: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]) + '\n')
-			temp_state = curr_state
-			hist_loc[str(temp_state[0])] = temp_state[1:]
-		time.sleep(1)
-
-def sub(lock, curr_state):
-	while 1:
-		with lock:
-			curr_state[0] = curr_state[0]-5
-			print('Sub: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]) + '\n') 
-		time.sleep(1)
-'''
 def localization_wifi(curr_state, lock, hist_loc, map_dict, list_dict_point):
 	try:
+		'''
 		scan_states = [0, 0, 0]
 		scan_states[0] = curr_state[0]
 		scan_states[1] = curr_state[1]
 		scan_states[2] = curr_state[2]
+		'''
 		while 1:
-			#scan_states = wifi_localization_once.wifi_localization_once(map_dict, list_dict_point) # return [x, y, time]
+			scan_states = wifi_localization_once.wifi_localization_once(map_dict, list_dict_point) # return [x, y, time]
+			'''
 			scan_states[0] = time.time()
 			scan_states[1] = scan_states[1]+3
 			scan_states[2] = scan_states[1]+5
-
 			'''
-			with lock:
-				curr_state[0] = scan_states[0]
-				curr_state[1] = scan_states[1]
-				print('Wifi: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]) + '\n')
-				#hist_loc[str(scan_states[0])] = scan_states[1:]
-				#print hist_loc
-			'''
-			print('Wifi scan: '+ str(scan_states[0]) + ' ' + str(scan_states[1]) + ' ' + str(scan_states[2]) + '\n')
+			print('Wifi scan: '+ str(scan_states[0]) + ' ' + str(scan_states[1]) + ' ' + str(scan_states[2])+'\n')
 			time.sleep(2)
+
 			#adjustment
 			localization_adjustment.localization_adjustment(curr_state, lock, hist_loc, scan_states)
 			time.sleep(1)
@@ -66,15 +44,18 @@ def localization_wifi(curr_state, lock, hist_loc, map_dict, list_dict_point):
 
 def localization_IMU(curr_state, lock, hist_loc, conn):
 	while 1:
-		#movements = server.server_cont(conn)
-		movements = [time.time(), 2, 3]
+		movements = server.server_cont(conn)
+		print 'movements: '
+		#movements = [time.time(), 2, 3]
 		print movements
 		with lock:
 			curr_state[0] = movements[0]
 			curr_state[1] = curr_state[1]+movements[1]
 			curr_state[2] = curr_state[2]+movements[2]
-			print('IMU: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]) + '\n')
 			hist_loc[str(curr_state[0])] = curr_state[1:]
+			print('******	Current	states	******')
+			print('IMU: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]))
+			print('********************************\n')
 		time.sleep(0.5)
 
 
@@ -83,7 +64,7 @@ def localization_IMU(curr_state, lock, hist_loc, conn):
 print('Welcome to Localization\n')
 #Initialization
 [map_dict,list_dict_point] = Loc_wifi_ini.Loc_wifi_ini()
-#conn = server.server_ini()
+conn = server.server_ini()
 os.system('chmod +x testPoint.sh')
 
 # create shared variables
@@ -96,24 +77,18 @@ curr_state = Array('d', starting_loc)
 conn = 1 
 p_wifi = partial(localization_wifi, curr_state, lock, hist_loc, map_dict, list_dict_point)
 p_IMU = partial(localization_IMU, curr_state, lock, hist_loc, conn)
-#p_add = partial(add, lock, curr_state)
-#p_sub = partial(sub, lock, curr_state)
-
 
 pro_wifi = Process(name = 'pro_wifi', target=p_wifi)
 pro_IMU = Process(name='pro_IMU', target=p_IMU)
-#pro_add = Process(name = 'pro_add', target = p_add)
-#pro_sub = Process(name = 'pro_sub', target = p_sub)
 
+
+# processes start
 pro_wifi.start()
 pro_IMU.start()
-#pro_add.start()
-#pro_sub.start()
 
 pro_wifi.join()
 pro_IMU.join()
-#pro_add.join()
-#pro_sub.join()
+
 
 
 
--- localization/localization_adjustment.py
+++ localization/localization_adjustment.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 
 def get_nearest_less_element(d, k):
     return d[str(max(key for key in map(float, d.keys()) if key <= k))]
@@ -9,8 +9,7 @@ def localization_adjustment(curr_state, lock, hist_loc, scan_states):
 	print 'scan_states: '
 	print scan_states
 	scan_time = scan_states[0]
-	scan_time = scan_time+0.5 # add uncertainties
-	print('Adjust current states: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]) + '\n')	
+	print('Adjust current states: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]))	
 	print hist_loc
 	print scan_time
 	with lock:
@@ -21,16 +20,25 @@ def localization_adjustment(curr_state, lock, hist_loc, scan_states):
 
 	#calculate kalman_gain (how much do we trust the adjustment and wifi localization)
 	kalman_gain = 0.6
+	with lock:
+		temp_time = curr_state[0]
+		temp_x = curr_state[1]
+		temp_y = curr_state[2]
+	rate_change = math.sqrt(math.pow((scan_states[1]-temp_x),2)+math.pow((scan_states[2]-temp_y),2)/math.pow((temp_time - scan_states[0]),2))
+	print rate_change
+	kalman_gain = math.exp(-1*rate_change/16)
+	print 'kalman_gain'
+	print kalman_gain
 
 	#adjust current location based on scan_wifi(before)
 	adjust_amount = np.subtract(matched_loc, scan_states[1:])
 	print 'adjust_amount: '
 	print adjust_amount
-	print('before adjustment for current states: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]) + '\n')
+	print('before adjustment for current states: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]))
 	with lock:
 		curr_state[1] = curr_state[1]+adjust_amount[0]*kalman_gain
 		curr_state[2] = curr_state[2]+adjust_amount[1]*kalman_gain
-	print('after adjustment for current states: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]) + '\n')
+	print('after adjustment for current states: '+ str(curr_state[0]) + ' ' + str(curr_state[1]) + ' ' + str(curr_state[2]))
 	print matched_loc
 
 
